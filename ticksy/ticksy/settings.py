64a65e5fb47281cc73d1d0256a5cfeee487af3b4
decemyn <radulescudecebal@outlook.com>
2022-03-26 17:37:48 +0200

    RAC-4 - Update database to MariaDB

--- ticksy/ticksy/settings.py
+++ ticksy/ticksy/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'ticksy.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ticksy_cmdb',
+        'USER': 'ticksy_dev',
+        'PASSWORD': 'dev',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 

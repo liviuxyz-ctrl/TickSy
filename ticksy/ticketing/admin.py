65c30fa97820146033ec38639a80aaf9477a5901
Decebal <radulescudecebal@outlook.com>
2022-04-30 22:34:12 +0300

    RAC-7 - Registered admin models

--- ticksy/ticketing/admin.py
+++ ticksy/ticketing/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import EmployeesPrivateData, Employees, Teams, Tickets
 
-# Register your models here.
+admin.site.register(Employees)
+admin.site.register(EmployeesPrivateData)
+admin.site.register(Teams)
+admin.site.register(Tickets)

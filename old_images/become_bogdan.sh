fc8aee0aa548d0845ed687164cf2496b73a9752e
decemyn <decebal.radulesc02u@e-uvt.ro>
2022-12-14 21:23:27 +0200

    Cand m-am inbatat cu git am facut un commit cu un nume gresit.

---
+++ old_images/become_bogdan.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+git config --global  user.name Bogdan Bozga;
+git config --global  user.email bogdan.bozga01@gmail.com
+
+
---
+++ old_images/become_liviu.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+git config --global  user.name liviuxyz-ctrl;
+git config --global  user.email liviu2002liviu@gmail.com;
+
+# cand m-am inbatat cu git am facut un commit cu un nume gresit.
+
+
+

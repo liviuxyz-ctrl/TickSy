8d492ee015d65f0ee9ddd4e47c53b820355f0a73
Decebal <radulescudecebal@outlook.com>
2023-01-16 21:29:48 +0200

    RAC-9 - Added login check for API function

--- ticksy/ticketing/fast_api/api.py
+++ ticksy/ticketing/fast_api/api.py
@@ -1,7 +1,9 @@
 from django.http import JsonResponse
 from fastapi import FastAPI
 from ..models import Tickets
+from ..views import logger
 from django.shortcuts import get_object_or_404
+
 
 app = FastAPI()
 
@@ -19,17 +21,33 @@ class TicketInfo(Tickets):
 
 @app.get("/ticket/{ticket_id}")
 def lookup_ticket(request, ticket_id : int):
-    ticket = get_object_or_404(Tickets, id=ticket_id)
-    ticket_information = {
-        "user_full_name": ticket.user_full_name,
-        "user_email": ticket.user_email,
-        "created_at": ticket.created_at,
-        "due_datetime": ticket.due_datetime,
-        "finish_at": ticket.finish_at,
-        "status": ticket.status,
-        "importance": ticket.importance,
-        "responsible_team_id": ticket.responsible_team_id.id,
-        "responsible_employee_id": ticket.responsible_employee_id.id,
-        "description": ticket.description
-    }
-    return JsonResponse(data=ticket_information)
+    if request.session.get('login_state', default=False):
+
+        try:
+            ticket = Tickets.objects.get(id=ticket_id)
+            ticket_information = {
+                "user_full_name": ticket.user_full_name,
+                "user_email": ticket.user_email,
+                "created_at": ticket.created_at,
+                "due_datetime": ticket.due_datetime,
+                "finish_at": ticket.finish_at,
+                "status": ticket.status,
+                "importance": ticket.importance,
+                "responsible_team_id": ticket.responsible_team_id.id,
+                "responsible_employee_id": ticket.responsible_employee_id.id,
+                "description": ticket.description
+            }
+        except:
+            ticket_information = {
+                "ticket_lookup_failure":  True,
+                "reason": f"No ticket with id '{ticket_id}' found!"
+            }
+
+        return JsonResponse(data=ticket_information)
+
+    else:
+        logger.debug(f"Team lookup deny, user not logged in!")
+        api_deny_response = {
+            "ticket_lookup_deny": True
+        }
+        return JsonResponse(data=api_deny_response)
